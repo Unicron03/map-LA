0d509073f1ce114a3d804a8576c2220995a06530
Johnmclf <150351096+Johnmclf@users.noreply.github.com>
2025-01-11 15:14:08 +0100

    Résolution bug doublon adresse mail

--- scripts/management/account/inscription.php
+++ scripts/management/account/inscription.php
@@ -12,14 +12,25 @@ if (isset($_POST['register'])) {
     // ---------------------Cryptage du mot de passe-------------------------------------------------------------------
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-    //----------------------Insertion ---------------------------------------------------------------------------------
-    $stmt = $pdo->prepare("INSERT INTO users (username, password, email, fullname) VALUES (?, ?, ?, ?)");
-    if ($stmt->execute([$username, $password, $email, $fullname])) {
-        echo "<script>alert('Successful registration! Please log in.');</script>";
+    // ---------------------- Vérification si l'email existe déjà ----------------------------------------------------
+    $checkEmail = $pdo->prepare("SELECT COUNT(*) FROM users WHERE email = ?");
+    $checkEmail->execute([$email]);
+    $emailExists = $checkEmail->fetchColumn();
 
-        // --------------------Revenir au formulaire de Connection-------------------------------------------------------
-        echo "<script>document.addEventListener('DOMContentLoaded', function() { toggleForm('login-form'); });</script>"; 
+    if ($emailExists) {
+        // Si l'email existe déjà, afficher un message d'erreur
+        echo "<script>alert('These identifiers have already been taken. Please enter another.');</script>";
     } else {
-        echo "<script>alert('Registration error. Please try again.');</script>";
+        // ----------------------Insertion ---------------------------------------------------------------------------
+        $stmt = $pdo->prepare("INSERT INTO users (username, password, email, fullname) VALUES (?, ?, ?, ?)");
+        if ($stmt->execute([$username, $password, $email, $fullname])) {
+            echo "<script>alert('Successful registration! Please log in.');</script>";
+
+            // --------------------Revenir au formulaire de Connection------------------------------------------------
+            echo "<script>document.addEventListener('DOMContentLoaded', function() { toggleForm('login-form'); });</script>"; 
+        } else {
+            echo "<script>alert('Registration error. Please try again.');</script>";
+        }
     }
 }
+?>
